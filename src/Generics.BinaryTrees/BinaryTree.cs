9d638f3fc79cb376700c8986d981ed241ccf669b
Andrew <andrew.1.kulikov@gmail.com>
2020-10-17 10:57:26 +0300

    Add binary tree solution

--- src/Generics.BinaryTrees/BinaryTree.cs
+++ src/Generics.BinaryTrees/BinaryTree.cs
@@ -1,10 +1,77 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Generics.BinaryTrees
 {
-    
+    public class BinaryTree<T> : IEnumerable<T> where T : IComparable<T>
+    {
+        private bool _initialized;
+        private T _value;
+
+        public T Value
+        {
+            get => _value;
+            private set
+            {
+                _initialized = true;
+                _value = value;
+            }
+        }
+
+        public BinaryTree<T> Left { get; private set; }
+        public BinaryTree<T> Right { get; private set; }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            if (!_initialized) yield break;
+
+            if (Left != null)
+                foreach (var leftItem in Left)
+                    yield return leftItem;
+
+            yield return Value;
+
+            if (Right != null)
+                foreach (var rightItem in Right)
+                    yield return rightItem;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(T value)
+        {
+            if (!_initialized)
+            {
+                Value = value;
+                return;
+            }
+
+            if (value.CompareTo(Value) <= 0)
+            {
+                if (Left == null) Left = new BinaryTree<T>();
+                Left.Add(value);
+            }
+            else
+            {
+                if (Right == null) Right = new BinaryTree<T>();
+                Right.Add(value);
+            }
+        }
+    }
+
+    public class BinaryTree
+    {
+        public static BinaryTree<T> Create<T>(params T[] items) where T : IComparable<T>
+        {
+            var tree = new BinaryTree<T>();
+
+            foreach (var item in items) tree.Add(item);
+
+            return tree;
+        }
+    }
 }
