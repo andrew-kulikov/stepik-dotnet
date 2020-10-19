31c5e3f3247e407e960d3e53eadb2bb1e1fca225
Andrew <andrew.1.kulikov@gmail.com>
2020-10-19 23:23:47 +0300

    Add observer task with solution

---
+++ src/Delegates.Observers/ObservableStack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegates.Observers
+{
+    public class StackOperationsLogger
+    {
+        private readonly StringBuilder _log = new StringBuilder();
+
+        public void SubscribeOn<T>(ObservableStack<T> stack)
+        {
+            stack.ItemChanged += data => _log.Append(data);
+        }
+
+        public string GetLog()
+        {
+            return _log.ToString();
+        }
+    }
+
+    public class ObservableStack<TItem>
+    {
+        private readonly IList<TItem> data = new List<TItem>();
+
+        public event Action<StackEventData<TItem>> ItemChanged;
+
+        public void Push(TItem obj)
+        {
+            data.Add(obj);
+            ItemChanged?.Invoke(new StackEventData<TItem> {IsPushed = true, Value = obj});
+        }
+
+        public TItem Pop()
+        {
+            if (data.Count == 0) throw new InvalidOperationException();
+
+            var result = data[data.Count - 1];
+            ItemChanged?.Invoke(new StackEventData<TItem> {IsPushed = false, Value = result});
+
+            return result;
+        }
+    }
+}
