aeb5364076ba005e373d5128d17030e38cb5bf77
Andrew <andrew.1.kulikov@gmail.com>
2020-11-01 17:51:38 +0300

    Add APIObject task

---
+++ src/Memory.API.csproj/APIObject.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Memory.API
+{
+    public class APIObject : IDisposable
+    {
+        private readonly int n;
+
+        public APIObject(int n)
+        {
+            this.n = n;
+
+            MagicAPI.Allocate(n);
+        }
+
+        #region IDisposable Support
+
+        private bool disposed;
+
+        public void Dispose()
+        {
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+
+        ~APIObject()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose(bool disposing)
+        {
+            if (disposed) return;
+
+            MagicAPI.Free(n);
+            disposed = true;
+        }
+
+        #endregion
+    }
+}
